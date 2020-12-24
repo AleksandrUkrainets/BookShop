acd70348fb8fcf99a1d1ad2b7b5c00bf92414f48
Aleksandr <ukrainets.alexander@gmail.com>
2020-12-24 21:25:54 +0200

    add SearchController

--- infrastructure/StoreMemory/BookRepository.cs
+++ infrastructure/StoreMemory/BookRepository.cs
@@ -7,13 +7,13 @@ namespace StoreMemory
     {
         private readonly Book[] _books = new[]
         {
-            new Book(1, "Art of programming"),
+            new Book(1, "Art of Programming"),
             new Book(2, "Refactoring"),
             new Book(3, "C Programming Language"),
         };
         public Book[] GetAllByTitle(string titlePart)
         {
-            return _books.Where(book => book.Title.Equals(titlePart)).ToArray();
+            return _books.Where(book => book.Title.Contains(titlePart)).ToArray();
         }
     }
 }
